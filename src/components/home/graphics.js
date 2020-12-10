51b7d96678d963fb47e33683ee7a0a3d7cfb17b2
Oscar Sidebo <sideboo@gmail.com>
2020-12-09 20:12:21 -0800

    .

--- src/components/home/graphics.js
+++ src/components/home/graphics.js
@@ -9,56 +9,73 @@ const Graphics = () => {
         variant: "pages.home.top.bars",
       }}
     >
-      <motion.div
-        initial={{ opacity: 1 }}
-        animate={{ opacity: 1 }}
-        transition={{
-          staggerChildren: 10,
-        }}
-        className="barWrapper"
-      >
-        <motion.span
-          // initial={{
-          //   top: "250px",
-          //   height: "0",
-          // }}
-          // animate={{
-          //   top: 0,
-          //   height: "250px",
-          // }}
-          // transition={{
-          //   duration: 0.5,
-          // }}
-          id="bar1"
-        ></motion.span>
-        <motion.span
-          // initial={{
-          //   top: "250px",
-          //   height: "0",
-          // }}
-          // animate={{
-          //   top: "97px",
-          //   height: "153px",
-          // }}
-          // transition={{
-          //   duration: 0.5,
-          // }}
-          id="bar2"
-        ></motion.span>
-        <motion.span
-          // initial={{
-          //   top: "250px",
-          //   height: "0",
-          // }}
-          // animate={{
-          //   top: "175px",
-          //   height: "75px",
-          // }}
-          // transition={{
-          //   duration: 0.5,
-          // }}
-          id="bar3"
-        ></motion.span>
+      <motion.div className="barWrapper">
+        <motion.div id="bar1">
+          <motion.div
+            initial={{
+              height: 0,
+            }}
+            animate={{
+              height: "100%",
+            }}
+            transition={{
+              duration: 0.5,
+            }}
+            className="top"
+          ></motion.div>
+          <motion.div
+            initial={{
+              height: 0,
+            }}
+            animate={{
+              height: "40%",
+            }}
+            transition={{
+              duration: 0.5,
+            }}
+            className="bottom"
+          ></motion.div>
+        </motion.div>
+        <motion.div id="bar2">
+          <motion.div
+            initial={{
+              height: 0,
+            }}
+            animate={{
+              height: "61%",
+            }}
+            transition={{
+              duration: 0.5,
+            }}
+            className="top"
+          ></motion.div>
+          <motion.div
+            initial={{
+              height: 0,
+            }}
+            animate={{
+              height: "40%",
+            }}
+            transition={{
+              duration: 0.5,
+            }}
+            className="bottom"
+          ></motion.div>
+        </motion.div>
+        <motion.div id="bar3">
+          <motion.div
+            initial={{
+              height: 0,
+            }}
+            animate={{
+              height: "40%",
+            }}
+            transition={{
+              duration: 0.5,
+            }}
+            className="bottom"
+          ></motion.div>
+        </motion.div>
       </motion.div>
     </div>
   );
