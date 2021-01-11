691bc8eaf96855fe3d14314ac001edcf57ad42a9
Oscar Sidebo <sideboo@gmail.com>
2021-01-11 13:31:58 -0800

    .

--- src/components/home/quotes.js
+++ src/components/home/quotes.js
@@ -41,6 +41,10 @@ const Quotes = ({
       },
     ]);
   };
+  const swipeConfidenceThreshold = 10000;
+  const swipePower = (offset, velocity) => {
+    return Math.abs(offset) * velocity;
+  };
 
   useEffect(() => {
     const id = setTimeout(() => paginate(1), time);
@@ -69,6 +73,21 @@ const Quotes = ({
               initial="enter"
               animate="center"
               exit="exit"
+              drag="x"
+              dragConstraints={{
+                left: 0,
+                right: 0,
+              }}
+              dragElastic={1}
+              onDragEnd={(e, { offset, velocity }) => {
+                const swipe = swipePower(offset.x, velocity.y);
+
+                if (swipe < -swipeConfidenceThreshold) {
+                  paginate(1);
+                } else if (swipe > swipeConfidenceThreshold) {
+                  paginate(-1);
+                }
+              }}
             >
               <motion.source
                 media="(min-width: 800px)"
