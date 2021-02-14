0bc2a427c388d37fdf62e93fdc79ab323a82c696
Oscar Sidebo <sideboo@gmail.com>
2021-02-14 12:43:30 -0800

    .

--- src/components/home/partners.js
+++ src/components/home/partners.js
@@ -10,8 +10,7 @@ import InView from "../inview";
 import theme from "../../../theme";
 import { rootMargin } from "../../helpers/utils";
 import { useInView } from "react-intersection-observer";
-import CarouselRow from './carouselrow'
-
+import CarouselRow from "./carouselrow";
 
 const Partners = ({
   setNavbarStyling,
@@ -19,10 +18,9 @@ const Partners = ({
   navBarStyling,
   isDesktop,
   content,
-  isServer
+  isServer,
 }) => {
   const { header, logosRow1, logosRow2 } = content;
-
 
   return (
     <InView
@@ -41,12 +39,12 @@ const Partners = ({
           <Styled.h2 className="Partners__text-header">{header}</Styled.h2>
         </div>
         <div className="Partners__logoCarousel">
-              <CarouselRow key={0} logosRow={logosRow1} duration={60} isServer={isServer} additionalClass={`row1`} />
-              <CarouselRow key={1} logosRow={logosRow2} duration={320} isServer={isServer} additionalClass={`row2`} />
+          {/* <CarouselRow key={0} logosRow={logosRow1} duration={60} isServer={isServer} additionalClass={`row1`} />
+              <CarouselRow key={1} logosRow={logosRow2} duration={320} isServer={isServer} additionalClass={`row2`} /> */}
         </div>
       </div>
     </InView>
-   );
-  }
+  );
+};
 
-export default Partners
+export default Partners;
