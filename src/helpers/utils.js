2be9c3949f962e7a20105f350ada373dd2999baa
Oscar Sidebo <sideboo@gmail.com>
2021-03-07 09:30:53 -0800

    reduce namefield focus delay

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -39,7 +39,7 @@ export const scrollToBottom = (window, isDesktop) => {
       // click on input field
       const nameField = window.document.getElementById("namefield");
       nameField.focus();
-    }, 1500);
+    }, 500);
   } else {
     // const scrollTarget = window.document
     //   .querySelector("#footerHeader")
